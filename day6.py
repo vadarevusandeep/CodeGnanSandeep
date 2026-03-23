5a12b800cd7bf4560e9f793419b6a7bd24bc13cb
vadarevusandeep <sandeepvadarevu@gmail.com>
2026-03-23 13:28:38 +0530

    conditional statements

---
+++ day6.py
@@ -0,0 +1,86 @@
+'''
+if statement
+if else statement
+if-elif-else statement
+code Q
+
+'''
+name  = input("Enter your name : ")
+'''
+number = int (input("Enter any number : "))
+if number <= 10:
+       print ("your number is 1 or above")
+student_att = int (input("Pls enter your sem attendance"))
+if student_att >= 75:
+    print ("You can directly in the sem exam")
+'''
+'''
+age = int (input("Enter your age : "))
+if age >= 18:
+    print ("Adult")
+
+
+password = input ("Enter Password: ")
+
+if password == "1234":
+            print ("Correct password")
+            '''           
+'''
+total_am = int ("Enter the total shopping money : "))
+if total_am >= 149:
+    print ("No deliver cost")
+    else:
+        print(f"add {149 - total_am } to your cart ")
+'''
+
+'''if-elif-else statement (if + else)--- in the elif part,  I can more condition'''
+'''
+bill_input = int(input("Enter Bill Amount : "))
+if bill_input >= 1000 and bill_input <2000 :
+                 print ("hey you got 10% discount")
+elif bill_input >= 2000 and bill_input <2500 :
+                 print ("hey you got 20% discount")
+elif bill_input >= 2500 :
+                print ("You unlocked Vip subscription")
+print ("Thank your For Shopping")
+'''
+
+'''
+num_1 = int(input("enter  1st number"))
+num_2 = int(input("enter  2nd number"))
+user_choice = input("enter your choice")
+if user_choice == "+":
+    print(f"{num_1 + num_2}")
+elif user_choice == "-":
+    print(f"{num_1 - num_2}")'''
+
+
+'''
+num_1 = int(input("enter  1st number"))
+num_2 = int(input("enter  2nd number"))
+user_choice = input("enter your choice\n 1.add \n 2.sub\n 3.mul\n 4.div\n")
+if user_choice == "1":
+    print(f"{num_1 + num_2}")
+elif user_choice == "2":
+    print(f"{num_1 - num_2}")
+elif user_choice == "3":
+    print(f"{num_1 * num_2}")
+else:
+    print(f"{num_1 / num_2}")
+    '''
+user_input=int(input("enter any digit : "))
+if user_input > 0 :
+     print ("Entered Digit is positive : ")
+elif user_input< 0 :
+     print ("Entered Digit is negative : ")
+else :
+     print ("Entered Digit is Zero : " )
+
+
+
+
+
+                 
+    
+                   
+              
